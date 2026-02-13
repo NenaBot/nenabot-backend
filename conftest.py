3dd5ccb679aca4f818c2de96070215b19599d3a0
Jean-Baptiste Van Parys <jean-baptiste.vanparys@tuni.fi>
2026-02-13 17:19:10 +0200

    edit test config

--- conftest.py
+++
@@ -1,8 +0,0 @@
-"""Root pytest configuration"""
-import sys
-from pathlib import Path
-
-# Add subdirectory packages to Python path for imports
-root_dir = Path(__file__).parent
-sys.path.insert(0, str(root_dir / "camera_detection"))
-sys.path.insert(0, str(root_dir / "orchestrator"))
